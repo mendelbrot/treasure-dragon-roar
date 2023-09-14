f8a9d66fcdcaae6833f19b7ec004cfaf7f7f2253
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-09-13 21:55:31 -0700

    renders dragon in center

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new().add_plugins(DefaultPlugins)
+    .add_systems(Startup, setup).run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("characters/dragon1_md.png"),
+        ..default()
+    });
 }
