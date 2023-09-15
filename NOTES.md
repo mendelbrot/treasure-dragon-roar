87bf1f3364385d90452bef72bdc5f8ed8a686077
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-09-14 21:49:47 -0700

    improved scaling and movement.  aded music

--- NOTES.md
+++ NOTES.md
@@ -8,6 +8,8 @@ cargo run --features bevy/dynamic_linking
 
 [bevy book](https://bevyengine.org/learn/book/getting-started/ecs/)
 
+[bevy cheatbook](https://bevy-cheatbook.github.io/features/coords.html)
+
 [bevy examples](https://github.com/bevyengine/bevy/tree/latest/examples#examples)
 
 [rust book](https://doc.rust-lang.org/book/)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::ops::Add;
+use std::ops::Mul;
+
+use bevy::audio::PlaybackMode::Loop;
 use bevy::prelude::*;
 
 fn main() {
@@ -5,6 +9,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .init_resource::<Game>()
         .add_systems(Startup, setup)
+        .add_systems(Update, gizmo_shapes)
         .add_systems(Update, move_dragon)
         .run();
 }
@@ -12,8 +17,7 @@ fn main() {
 #[derive(Default)]
 struct Dragon {
     entity: Option<Entity>,
-    x: usize,
-    y: usize,
+    position_unscaled: Vec3,
     move_cooldown: Timer,
 }
 
@@ -22,25 +26,36 @@ struct Game {
     dragon: Dragon,
 }
 
-const MAP_SIZE_X: usize = 21;
-const MAP_SIZE_Y: usize = 21;
+const MAP_RADIUS: f32 = 30.;
+const MAP_SCALE: f32 = 15.;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMut<Game>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(AudioBundle {
+const COOL_DOWN: f32 = 0.1;
+
+fn setup(mut _commands: Commands, asset_server: Res<AssetServer>, mut game: ResMut<Game>) {
+    _commands.spawn(Camera2dBundle::default());
+    _commands.spawn(AudioBundle {
         source: asset_server.load("sounds/windless_slopes.ogg"),
+        settings: PlaybackSettings {
+            mode: Loop,
+            ..default()
+        },
         ..default()
     });
 
     // spawn the dragon
-    game.dragon.x = MAP_SIZE_X / 2;
-    game.dragon.y = MAP_SIZE_Y / 2;
-    game.dragon.move_cooldown = Timer::from_seconds(0.3, TimerMode::Once);
+    game.dragon.position_unscaled = Vec3 {
+        x: 0.,
+        y: 0.,
+        z: 0.,
+    };
+    game.dragon.move_cooldown = Timer::from_seconds(COOL_DOWN, TimerMode::Once);
     game.dragon.entity = Some(
-        commands
+        _commands
             .spawn(SpriteBundle {
                 texture: asset_server.load("objects2d/dragon1_md.png"),
-                transform: Transform::from_xyz(game.dragon.x as f32, game.dragon.y as f32, 0.),
+                transform: Transform::from_translation(
+                    game.dragon.position_unscaled.mul(MAP_SCALE),
+                ),
                 ..default()
             })
             .id(),
@@ -56,28 +71,49 @@ fn move_dragon(
 ) {
     if game.dragon.move_cooldown.tick(time.delta()).finished() {
         let mut moved = false;
+        let mut next_position_unscaled = game.dragon.position_unscaled;
 
         if keyboard_input.pressed(KeyCode::Up) {
-            if game.dragon.y < MAP_SIZE_Y - 1 {
-                game.dragon.y += 1;
+            next_position_unscaled = game.dragon.position_unscaled.add(Vec3 {
+                x: 0.,
+                y: 1.,
+                z: 0.,
+            });
+            if next_position_unscaled.distance(Vec3::ZERO) <= MAP_RADIUS {
+                game.dragon.position_unscaled = next_position_unscaled
             }
             moved = true;
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            if game.dragon.y > 0 {
-                game.dragon.y -= 1;
+            next_position_unscaled = game.dragon.position_unscaled.add(Vec3 {
+                x: 0.,
+                y: -1.,
+                z: 0.,
+            });
+            if next_position_unscaled.distance(Vec3::ZERO) <= MAP_RADIUS {
+                game.dragon.position_unscaled = next_position_unscaled
             }
             moved = true;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            if game.dragon.x < MAP_SIZE_X - 1 {
-                game.dragon.x += 1;
+            next_position_unscaled = game.dragon.position_unscaled.add(Vec3 {
+                x: 1.,
+                y: 0.,
+                z: 0.,
+            });
+            if next_position_unscaled.distance(Vec3::ZERO) <= MAP_RADIUS {
+                game.dragon.position_unscaled = next_position_unscaled
             }
             moved = true;
         }
         if keyboard_input.pressed(KeyCode::Left) {
-            if game.dragon.x > 0 {
-                game.dragon.x -= 1;
+            next_position_unscaled = game.dragon.position_unscaled.add(Vec3 {
+                x: -1.,
+                y: 0.,
+                z: 0.,
+            });
+            if next_position_unscaled.distance(Vec3::ZERO) <= MAP_RADIUS {
+                game.dragon.position_unscaled = next_position_unscaled
             }
             moved = true;
         }
@@ -85,24 +121,14 @@ fn move_dragon(
         // move on the board
         if moved {
             game.dragon.move_cooldown.reset();
-            *transforms.get_mut(game.dragon.entity.unwrap()).unwrap() = Transform {
-                translation: Vec3::new(
-                    game.dragon.x as f32,
-                    game.dragon.y as f32,
-                    0.
-                ),
-                ..default()
-            };
+            *transforms.get_mut(game.dragon.entity.unwrap()).unwrap() =
+                Transform::from_translation(game.dragon.position_unscaled.mul(MAP_SCALE))
         }
     }
 }
 
-// fn handle_action(time: Res<Time>, mut char_input_events: EventReader<ReceivedCharacter>) {
-//     // timer.0.reset();
-//     for event in char_input_events.iter() {
-//         info!("{:?}: '{}'", event, event.char);
-//     }
-//     if timer.0.tick(time.delta()).just_finished() {
-//         info!("Hello!");
-//     }
-// }
+fn gizmo_shapes(mut gizmos: Gizmos) {
+    // debug shapes
+    gizmos.circle_2d(Vec2::ZERO, (MAP_RADIUS * MAP_SCALE) as f32, Color::BLUE);
+    gizmos.circle_2d(Vec2::ZERO, 5., Color::GREEN);
+}
